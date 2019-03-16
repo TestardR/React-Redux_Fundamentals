a39fc1cad0192345e68ee4a2f33fb1fcc4d4c110
unknown <rmtestard@gmail.com>
2019-03-16 14:15:58 +0100

    using props

--- components/src/index.js
+++ components/src/index.js
@@ -1,14 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
 
 import CommentDetail from './components/CommentDetail';
 
 const App = () => {
   return (
     <div>
-      <CommentDetail author="Vanessa" />
-      <CommentDetail author="Alex" />
-      <CommentDetail author="Romain" />
+      <CommentDetail
+        avatar={faker.image.avatar()}
+        author="Vanessa"
+        timeAgo="Today at 11:45AM"
+        content="Hey you cute!"
+      />
+      <CommentDetail
+        avatar={faker.image.avatar()}
+        author="Alex"
+        timeAgo="Yesterday at 1:23PM"
+        content="No way ?!"
+      />
+      <CommentDetail
+        avatar={faker.image.avatar()}
+        author="Romain"
+        timeAgo="Friday at 3:16PM"
+        content="hihihihi!"
+      />
     </div>
   );
 };
