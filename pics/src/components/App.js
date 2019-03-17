4ed832351003be7ae8c2853996df6c5fa9030ebf
unknown <rmtestard@gmail.com>
2019-03-17 18:41:31 +0100

    get request with axios

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
-const App = () => {
-  return (
-    <div className="ui container" style={{ marginTop: '10px' }}>
-      <SearchBar />
-    </div>
-  );
-};
+class App extends Component {
+  onSearchSubmit = term => {
+    axios.get('https://api.unsplash.com/search/photos', {
+      params: { query: term },
+      headers: {
+        Authorization:
+          'Client-ID 28aeda8703ec1c85f8a539c464cc26b613f73bf86d4383543c1c0ebad4849f70'
+      }
+    });
+  };
+
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: '10px' }}>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+      </div>
+    );
+  }
+}
 
 export default App;
