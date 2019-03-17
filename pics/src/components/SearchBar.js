2fd378da49f38143c326c89971e40ceeb227899e
unknown <rmtestard@gmail.com>
2019-03-17 17:12:48 +0100

    preventing default submissal

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+  state = { term: '' };
+
+  onInputChange = e => {
+    this.setState({ term: e.target.value });
+  };
+
+  onInputSubmit = e => {
+    e.preventDefault();
+  };
+
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form className="ui form" onSubmit={this.onInputSubmit}>
           <div className="field">
             <label>Image Search</label>
-            <input type="text" />
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={this.onInputChange}
+            />
           </div>
         </form>
       </div>
