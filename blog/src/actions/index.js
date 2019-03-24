53e1dd380487fb0291478f14be3232ab0144ec68
unknown <rmtestard@gmail.com>
2019-03-24 15:18:22 +0100

    using lodash to save overfetch request"

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,4 +1,19 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder';
+import _ from 'lodash';
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+
+  // const userIds = _.uniq(_.map(getState().posts, 'userId'));
+  // userIds.forEach(id => dispatch(fetchUser(id)));
+
+  // Alternative using lodash .chain
+  _.chain(getState().posts)
+    .map('userId')
+    .uniq()
+    .forEach(id => dispatch(fetchUser(id)))
+    .value();
+};
 
 export const fetchPosts = () => async dispatch => {
   const response = await jsonPlaceholder.get('/posts');
@@ -17,3 +32,17 @@ export const fetchUser = id => async dispatch => {
     payload: response.data
   });
 };
+
+// export const fetchUser = id => async dispatch => {
+//   _fetchUser(id, dispatch);
+// };
+
+// // memoization due to overfetching
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const response = await jsonPlaceholder.get(`/users/${id}`);
+
+//   dispatch({
+//     type: 'FETCH_USER',
+//     payload: response.data
+//   });
+// });
