d8c662e6a414a0822d5d62134c5b0e405dc15b1f
unknown <rmtestard@gmail.com>
2019-03-26 21:00:27 +0100

    sign in / sign out logic

--- streams/client/src/components/GoogleAuth.js
+++ streams/client/src/components/GoogleAuth.js
@@ -1,17 +1,34 @@
 import React, { Component } from 'react';
 
 class GoogleAuth extends Component {
+  state = { isSignedIn: null };
   componentDidMount() {
     window.gapi.load('client:auth2', () => {
-      window.gapi.client.init({
-        clientId:
-          '722389422854-qu61gfsc0aigj18p64deaibl6159qood.apps.googleusercontent.com',
-        scope: 'email'
-      });
+      window.gapi.client
+        .init({
+          clientId:
+            '722389422854-qu61gfsc0aigj18p64deaibl6159qood.apps.googleusercontent.com',
+          scope: 'email'
+        })
+        .then(() => {
+          this.auth = window.gapi.auth2.getAuthInstance();
+          this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+        });
     });
   }
+
+  renderAuthButton() {
+    if (this.state.isSignedIn === null) {
+      return <div>Loading...</div>;
+    } else if (this.state.isSignedIn) {
+      return <div>Signed in</div>;
+    } else {
+      return <div>Logged out</div>;
+    }
+  }
+
   render() {
-    return <div>Google Auth</div>;
+    return <div>{this.renderAuthButton()}</div>;
   }
 }
 
