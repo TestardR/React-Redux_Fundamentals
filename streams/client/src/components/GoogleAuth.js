c122f87192dc096e228970acb36d3d4adc5693e3
unknown <rmtestard@gmail.com>
2019-03-26 20:46:17 +0100

    Google Oauth initialization

--- streams/client/src/components/GoogleAuth.js
+++ streams/client/src/components/GoogleAuth.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
 
 class GoogleAuth extends Component {
+  componentDidMount() {
+    window.gapi.load('client:auth2', () => {
+      window.gapi.client.init({
+        clientId:
+          '722389422854-qu61gfsc0aigj18p64deaibl6159qood.apps.googleusercontent.com',
+        scope: 'email'
+      });
+    });
+  }
   render() {
     return <div>Google Auth</div>;
   }
