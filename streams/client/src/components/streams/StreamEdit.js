393e1e3dd1a7a8845c0c0f862b696c69aa0ada66
unknown <rmtestard@gmail.com>
2019-03-30 15:49:23 +0100

    on hydrating in edit form

--- streams/client/src/components/streams/StreamEdit.js
+++ streams/client/src/components/streams/StreamEdit.js
@@ -1,17 +1,34 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { fetchStream } from '../../actions';
+import { fetchStream, editStream } from '../../actions';
+import StreamForm from './StreamForm';
 
 class StreamEdit extends Component {
   componentDidMount() {
     this.props.fetchStream(this.props.match.params.id);
   }
+
+  onSubmit = formValues => {
+    console.log(formValues);
+  };
+
   render() {
     // console.log(this.props);
     if (!this.props.stream) {
       return <div>Loading...</div>;
     }
-    return <div>{this.props.stream.title}</div>;
+    return (
+      <div>
+        <h3>Edit a Stream</h3>
+        <StreamForm
+          initialValues={{
+            title: `${this.props.stream.title}`,
+            description: `${this.props.stream.description}`
+          }}
+          onSubmit={this.onSubmit}
+        />
+      </div>
+    );
   }
 }
 
@@ -22,5 +39,5 @@ const mapStateToprops = (state, ownProps) => {
 
 export default connect(
   mapStateToprops,
-  { fetchStream }
+  { fetchStream, editStream }
 )(StreamEdit);
