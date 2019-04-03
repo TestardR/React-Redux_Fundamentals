63bd73ca57f0fd72cfd26a4a2144b545ca014ac6
unknown <rmtestard@gmail.com>
2019-04-03 14:19:40 +0200

    accessing data with Consumer

--- translate/src/components/Button.js
+++ translate/src/components/Button.js
@@ -5,8 +5,14 @@ class Button extends Component {
   static contextType = LanguageContext;
   render() {
     // console.log(this.context);
-    const text = this.context === 'english' ? 'Submit' : 'Valider';
-    return <button className="ui button primary">{text}</button>;
+    // const text = this.context === 'english' ? 'Submit' : 'Valider';
+    return (
+      <button className="ui button primary">
+        <LanguageContext.Consumer>
+          {value => (value === 'english' ? 'Submit' : 'Valider')}
+        </LanguageContext.Consumer>
+      </button>
+    );
   }
 }
 
